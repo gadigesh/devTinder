c0c94c134c56de6e45341e8a8a17e93c1897c913
Gadigesh Hiremath <gchiremath.23@gmail.com>
2025-12-12 23:37:50 +0530

    multiple routes creation

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
 
-// app.use("/test", (req, res) => {
-// 	res.send("Dashboard");
-// });
-// app.use((req, res) => {
-// 	res.send("hello server");
-// });
-// app.use("/hellow", (req, res) => {
-// 	res.send("welome to hellow");
-// });
+app.use(
+	"/user",
+	(req, res, next) => {
+		//Route handler
 
-app.use("/gadi", (req, res) => {
-	res.send("welcome to gadigesh");
-});
+		//console.log("ahcgdf");
+		next();
+		//res.send("welome to hello");
+	},
+	(req, res) => {
+		//2nd route res
+		res.send("2 ndwelome to hello");
+		console.log("ahcgdf");
+	}
+);
 
 app.listen(7777, () => {
 	console.log("server started sucessfully 7777");
